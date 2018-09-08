3710d51abb4df4ed1d4c12989305183c4323fba4
Tyler Herbert <tyler@tylerherbert.ca>
2018-09-08 12:30:43 -0400

    remove bin

--- bin/Listener.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Listener {
-    listener: (NotificationInfo: any) => void;
-    identifier: string;
-    object?: any;
-    constructor(listener: (NotificationInfo: any) => void, identifier: string, object?: any);
-}
